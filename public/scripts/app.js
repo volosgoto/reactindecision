e89dda1d5f5e80e903cc0e2c8bce792031988c1f
volosgoto <volosgoto@yahoo.com>
2019-01-22 09:57:44 +0200

    add dinamic val

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,11 +12,91 @@
 
 //JSX syntax
 // let template = <p id="someid">Change check</p>;
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App!'
-);
-var appRoot = document.getElementById('app');
+// let template = (
+//   <div>
+//     <h1>Andrey</h1>
+//     <p>This is some text</p>
+//     <ol>
+//   <li>Item 1</li>
+//   <li>Item 2</li>
+//   <li>Item 3</li>
+// </ol>
+// </div>
+// );
 
+//-----------------------------------
+var user = {
+  name: 'Andrey',
+  age: 36,
+  location: 'Lincoln'
+  // let userName = 'Andrey';
+  // let userAge = 30;
+  // let userLocation = 'Lincoln';
+  // {} any Javscript expression inside
+
+};var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name.toUpperCase() + '!'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: ',
+    user.location
+  )
+);
+// --------------------------------------
+
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of computer'
+};
+
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item 1'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item 2'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item 3'
+    )
+  )
+);
+
+var appRoot = document.getElementById('app'); // root div
 ReactDOM.render(template, appRoot);
+// ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -11,7 +11,59 @@
 
 //JSX syntax
 // let template = <p id="someid">Change check</p>;
-let template = <h1>Indecision App!</h1>;
-let appRoot = document.getElementById('app');
+// let template = (
+//   <div>
+//     <h1>Andrey</h1>
+//     <p>This is some text</p>
+//     <ol>
+//   <li>Item 1</li>
+//   <li>Item 2</li>
+//   <li>Item 3</li>
+// </ol>
+// </div>
+// );
 
+//-----------------------------------
+let user = {
+  name: 'Andrey',
+  age: 36,
+  location: 'Lincoln'
+}
+// let userName = 'Andrey';
+// let userAge = 30;
+// let userLocation = 'Lincoln';
+// {} any Javscript expression inside
+
+let templateTwo = (
+  <div>
+    {/* <h1>{ alert('Hello')}</h1> */}  
+    {/* <h1>{ userName}</h1> */}  
+    <h1>{user.name.toUpperCase() + '!'}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+);
+// --------------------------------------
+
+
+let app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of computer'
+};
+
+let template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    <ol>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ol>
+  </div>
+);
+
+
+let appRoot = document.getElementById('app'); // root div
 ReactDOM.render(template, appRoot);
+// ReactDOM.render(templateTwo, appRoot);
