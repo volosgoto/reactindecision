41daf1a03e1c64db7f5d51e27897059b2acb79aa
volosgoto <volosgoto@yahoo.com>
2019-01-25 11:52:41 +0200

    es6 extends classes

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -21,10 +21,55 @@ class Person {
     return `${this.name} is ${this.age} years old`;
   }
 }
+// let me = new Person('Andew Vol', 36);
+// console.log(me.name);
+// console.log(me.getDescription());
 
-let me = new Person('Andew Vol', 36);
-console.log(me.name);
-console.log(me.getDescription());
+// let other = new Person();
+// console.log(other.getDescription());
 
-let other = new Person();
-console.log(other.getDescription());
+class Student extends Person{
+  constructor(name, age, major){
+    super(name, age) // calling a parent constructor
+    this.major = major;
+  }
+  hasMajor(){
+    return !!this.major;
+  }
+  getDescription(){
+    let description = super.getDescription();
+    // return description + this.major;
+      if(this.hasMajor()){
+        return description += ` .The major is ${this.major}`;
+      }
+  }
+}
+
+// let me = new Student('Andew Vol', 36, 'Computer');
+// console.log(me.getDescription());
+// // console.log(me);
+
+// let other = new Student();
+// // console.log(other.hasMajor());
+
+
+class Traveler extends Person{
+  constructor(name, age, homeLocation){
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+  getGreeting(){
+    let greeting = super.getGreeting();
+    if (this.homeLocation){
+      greeting += ` ${this.homeLocation}`;
+    }
+    return greeting;
+  }
+
+}
+
+let traveler = new Traveler('Sam', 30, 'Kiev');
+let t = new Traveler('Mike', 20);
+
+console.log(traveler.getGreeting());
+console.log(t.getGreeting());
