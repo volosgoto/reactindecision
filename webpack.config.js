dbcf84b6ab468d45f7fbdb8a8a3899c88cdcfa27
volosgoto <volosgoto@yahoo.com>
2019-01-31 09:35:05 +0200

    setup a webpack basic config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 // entry -> output
+const PATH = require('path');
 
 module.exports = {
-
+  entry : './src/app.js',
+  output : {
+    path : PATH.join(__dirname, 'public'),
+    filename : 'bundle.js'
+  },
 };
