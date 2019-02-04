1082c9c348672e103da1f22a45f6108f9bee442c
volosgoto <volosgoto@yahoo.com>
2019-02-04 15:46:32 +0200

    upt button styles

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from 'react'
 
 export default class AddOption extends React.Component {
   state = {
     error: undefined
-  };
+  }
   handleAddOption = (e) => {
-    e.preventDefault();
-    const option = e.target.elements.option.value.trim();
-    const error = this.props.handleAddOption(option);
+    e.preventDefault()
+    const option = e.target.elements.option.value.trim()
+    const error = this.props.handleAddOption(option)
 
-    this.setState(() => ({ error }));
+    this.setState(() => ({ error }))
 
     if (!error) {
-      e.target.elements.option.value = '';
+      e.target.elements.option.value = ''
     }
-  };
-  render() {
+  }
+  render () {
     return (
       <div>
         {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.handleAddOption}>
-          <input type="text" name="option" />
-          <button>Add Option</button>
+          <input type='text' name='option' />
+          <button className='button'>Add Option</button>
         </form>
       </div>
-    );
+    )
   }
 }
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from 'react'
 
 const Option = (props) => (
   <div>
     {props.optionText}
     <button
       onClick={(e) => {
-        props.handleDeleteOption(props.optionText);
+        props.handleDeleteOption(props.optionText)
       }}
+      className='button button--link'
     >
       remove
-      </button>
+    </button>
   </div>
-);
+)
 
-export default Option;
+export default Option
